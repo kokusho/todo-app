ef579267448d4e05a596d11fd2bcaf0aee755008
Julian <julian-brands@web.de>
2020-06-07 14:36:36 +0200

    added error messages when failing user validation

--- src/main/java/de/webtech/entities/User.java
+++ src/main/java/de/webtech/entities/User.java
@@ -1,6 +1,7 @@
 package de.webtech.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,19 +9,21 @@ import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
+
 @Entity
 public class User {
 
     @Id
     @Column(length = 30)
-    @NotNull
-    @Size(min = 4, max = 30)
+    @NotNull(message = "Username must be present.")
+    @Size(min = 4, max = 30, message = "Username must be longer than 4 characters and shorter than 30 characters.")
     private String username;
 
     @Column(length = 60)
-    @NotNull
-    @Size(min = 6, max = 60)
-    @JsonIgnore
+    @NotNull(message = "Password must be present.")
+    @Size(min = 6, max = 60, message = "Password must be longer than 6 characters and shorter than 60 characters.")
+    @JsonProperty(access = WRITE_ONLY) //property will be deserialized but not serialized
     private String password;
 
     public User(String username, String password) {
