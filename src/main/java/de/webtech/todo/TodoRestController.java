b5677384b4657f6183d00dbcf67a9097304fc8de
Julian <julian-brands@web.de>
2020-06-11 12:32:03 +0200

    fix

--- src/main/java/de/webtech/todo/TodoRestController.java
+++ src/main/java/de/webtech/todo/TodoRestController.java
@@ -3,8 +3,8 @@ package de.webtech.todo;
 import de.webtech.entities.Todo;
 import de.webtech.entities.User;
 import de.webtech.exceptions.BadRequestException;
+import de.webtech.shiro.SecurityUtilsWrapper;
 import de.webtech.user.UserRepository;
-import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +24,15 @@ public class TodoRestController {
     private static final transient Logger log = LoggerFactory.getLogger(TodoRestController.class);
 
     @Autowired
+    SecurityUtilsWrapper securityUtilsWrapper;
+    @Autowired
     private TodoRepository todoRepository;
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/")
     public Page<Todo> getTodos(@RequestParam(name = "p", defaultValue = "0") int page, @RequestParam(name = "i", defaultValue = "20") int pageSize ){
-        Optional<User> userOptional = userRepository.findById((String) SecurityUtils.getSubject().getPrincipal());
+        Optional<User> userOptional = userRepository.findById(securityUtilsWrapper.getPrincipal());
         if(userOptional.isEmpty()){
             return null; // TODO improve error handling
         }
@@ -42,7 +44,7 @@ public class TodoRestController {
         Todo todo = new Todo();
         todo.setTitle(todoTitle);
         todo.setDone(false);
-        Optional<User> userOptional = userRepository.findById((String) SecurityUtils.getSubject().getPrincipal());
+        Optional<User> userOptional = userRepository.findById(securityUtilsWrapper.getPrincipal());
         todo.setAssignedUser(userOptional.get());
         return this.todoRepository.save(todo);
     }
@@ -66,6 +68,21 @@ public class TodoRestController {
             return todoOpt.get();
         }
         return null; //TODO might wanna throw an exception
+    }
+
+    @PostMapping("/{id}")
+    public Todo reassignTodo(@PathParam("id") Long todoId, @RequestBody String newAssignee){
+        Optional<Todo> todoOpt = todoRepository.findById(todoId);
+        if(todoOpt.isEmpty()){
+            //TODO add error handling
+        }
+        Optional<User> newAssigneeOpt = userRepository.findById(newAssignee);
+        if(newAssigneeOpt.isEmpty()){
+            //TODO add even more error handling
+        }
+        Todo t = todoOpt.get();
+        t.setAssignedUser(newAssigneeOpt.get());
+        return todoRepository.save(t);
     }
 
     @GetMapping("/fakeData")
